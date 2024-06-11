15c3ec18e31c71021fca1e0660cd981d8d982ac3
lukemartinlogan <lukemartinlogan@gmail.com>
2024-06-11 01:54:58 -0500

    Use old style

--- include/chimaera/work_orchestrator/worker.h
+++ include/chimaera/work_orchestrator/worker.h
@@ -741,13 +741,12 @@ class Worker {
     }
 
     // Actually execute the task
-    ExecCoroutine(task, rctx);
-//    if (task->IsCoroutine()) {
-//      ExecCoroutine(task, rctx);
-//    } else {
-//      exec->Run(task->method_, task, rctx);
-//      task->SetStarted();
-//    }
+    if (task->IsCoroutine()) {
+      ExecCoroutine(task, rctx);
+    } else {
+      exec->Run(task->method_, task, rctx);
+      task->SetStarted();
+    }
     // Monitoring callback
     if (task->IsModuleComplete()) {
       exec->Monitor(MonitorMode::kEndTrainTime, task, rctx);
