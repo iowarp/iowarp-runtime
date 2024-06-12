7e724359d104fc9db17ac4c132a16adc9f15e113
lukemartinlogan <lukemartinlogan@gmail.com>
2024-06-12 01:38:41 -0500

    Fix make_task

--- ci/lint.sh
+++ ci/lint.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-HRUN_ROOT=$1
+CHIMEARA_ROOT=$1
 
 echo "RUNNING CPPLINT"
 cpplint --recursive \
-"${HRUN_ROOT}/src" "${HRUN_ROOT}/include" "${HRUN_ROOT}/test" \
---exclude="${HRUN_ROOT}/test/unit/external"
+"${CHIMEARA_ROOT}/src" "${CHIMEARA_ROOT}/include" "${CHIMEARA_ROOT}/test" \
+--exclude="${CHIMEARA_ROOT}/test/unit/external"
