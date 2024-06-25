b7457234a64cbdbde38d519d9fa9a2436f3c8798
lukemartinlogan <lukemartinlogan@gmail.com>
2024-06-25 03:48:17 -0500

    zlib bench

--- benchmark/test_zlib.cc
+++ benchmark/test_zlib.cc
@@ -8,14 +8,18 @@
 
 int main(int argc, char *argv[]) {
   MPI_Init(&argc, &argv);
-  size_t total_size = hshm::ConfigParse::ParseSize(argv[1]);
-  size_t io_size = hshm::ConfigParse::ParseSize(argv[2]);
-  char *path = argv[3];
-  size_t io_count = io_size / sizeof(int);
-  size_t iter = total_size / io_size;
+  int rank;
+  MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+  size_t block = hshm::ConfigParse::ParseSize(argv[1]);
+  size_t xfer = hshm::ConfigParse::ParseSize(argv[2]);
+  bool do_compress = std::stoi(argv[3]);
+  std::string basepath = argv[4];
+  std::string path = basepath + "/" + std::to_string(rank);
+  size_t xfer_count = xfer / sizeof(int);
+  size_t iter = block / xfer;
 
   // Open a file stdio
-  FILE *file = fopen(path, "w");
+  FILE *file = fopen(path.c_str(), "w");
   if (file == NULL) {
     fprintf(stderr, "Failed to open file\n");
     return 1;
@@ -24,7 +28,7 @@ int main(int argc, char *argv[]) {
   // Create a normal distribution
   hshm::NormalDistribution normal;
   normal.Shape(0, 512);
-  std::vector<int> data(io_count);
+  std::vector<int> data(xfer_count);
   normal.GetInt();
   for (size_t i = 0; i < iter; ++i) {
     data[i] = normal.GetInt();
@@ -32,12 +36,16 @@ int main(int argc, char *argv[]) {
 
   // Write compressed data repeatedly
   for (size_t i = 0; i < iter; ++i) {
-    hshm::Zlib compress;
-    size_t compressed_size = io_size;
-    std::vector<int> compressed(io_count);
-    compress.Compress(compressed.data(), compressed_size,
-                      data.data(), io_size);
-    fwrite(compressed.data(), sizeof(char), compressed_size, file);
+    if (do_compress) {
+      hshm::Zlib compress;
+      size_t compressed_size = xfer;
+      std::vector<int> compressed(xfer_count);
+      compress.Compress(compressed.data(), compressed_size,
+                        data.data(), xfer);
+      fwrite(compressed.data(), sizeof(char), compressed_size, file);
+    } else {
+      fwrite(data.data(), sizeof(char), xfer, file);
+    }
     fseek(file, 0, SEEK_SET);
   }
   MPI_Finalize();
