d65f1f3ce9941be7cb36645f7c06f045f521f640
lukemartinlogan <lukemartinlogan@gmail.com>
2024-12-08 06:55:38 -0600

    Remove python packages from spack deps

--- ci/load_deps.sh
+++ ci/load_deps.sh
@@ -10,12 +10,6 @@ cereal \
 boost@1.7: +context +fiber +coroutine +regex +system +filesystem +serialization +pic +math \
 mpi \
 python \
-py-pip \
-py-pybind11 \
-py-scipy \
-py-numpy \
-py-scikit-learn \
-py-pandas \
 lzo \
 bzip2 \
 zstd \
