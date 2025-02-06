924d73ffe977dc475851bc6e021913cac5232b63
lukemartinlogan <lukemartinlogan@gmail.com>
2025-02-06 05:32:27 -0600

    Ensure task methods is added

--- chimaera_codegen/chimaera_codegen/util/conv.py
+++
@@ -1,8 +0,0 @@
-
-def str_to_bool(text):
-    text = str(text).lower().strip()
-    if text == 'false':
-        return False
-    elif text == 'true':
-        return True
-    raise Exception("Neither true or false (str_too_bool)")
