e628744714d22bb0d91f37745ac6dff2c4f17dba
lukemartinlogan <lukemartinlogan@gmail.com>
2025-02-10 11:20:32 -0600

    Add chimaera_repo.yaml

--- CMake/ChimaeraCommonConfig.cmake
+++ CMake/ChimaeraCommonConfig.cmake
@@ -1,6 +1,6 @@
-#-----------------------------------------------------------------------------
+# -----------------------------------------------------------------------------
 # Find all packages needed by Chimaera
-#-----------------------------------------------------------------------------
+# -----------------------------------------------------------------------------
 # This is for compatability with SPACK
 SET(CMAKE_INSTALL_RPATH_USE_LINK_PATH TRUE)
 
@@ -21,7 +21,7 @@ if(BUILD_OpenMP_TESTS)
 endif()
 
 # Zeromq
-if (BUILD_ZeroMQ_TESTS)
+if(BUILD_ZeroMQ_TESTS)
     pkg_check_modules(ZMQ REQUIRED libzmq)
     include_directories(${ZMQ_INCLUDE_DIRS})
     message("Found libzmq at: ${ZMQ_INCLUDE_DIRS}")
@@ -29,7 +29,8 @@ endif()
 
 # Boost
 find_package(Boost REQUIRED COMPONENTS regex system filesystem fiber REQUIRED)
-if (Boost_FOUND)
+
+if(Boost_FOUND)
     message(STATUS "found boost at ${Boost_INCLUDE_DIRS}")
 endif()
 
@@ -37,8 +38,8 @@ endif()
 set(ALLOCATOR_LIBRARIES "")
 
 # jemalloc
-if (CHIMAERA_ENABLE_JEMALLOC)
-    pkg_check_modules (JEMALLOC jemalloc)
+if(CHIMAERA_ENABLE_JEMALLOC)
+    pkg_check_modules(JEMALLOC jemalloc)
     pkg_search_module(JEMALLOC REQUIRED jemalloc)
     include_directories(${JEMALLOC_INCLUDE_DIRS})
     link_directories(${JEMALLOC_LIBRARY_DIRS})
@@ -46,30 +47,34 @@ if (CHIMAERA_ENABLE_JEMALLOC)
 endif()
 
 # mimmalloc
-if (CHIMAERA_ENABLE_MIMALLOC)
+if(CHIMAERA_ENABLE_MIMALLOC)
     find_package(mimalloc REQUIRED)
-    if (mimalloc_FOUND)
+
+    if(mimalloc_FOUND)
         message(STATUS "found mimalloc at ${mimalloc_DIR}")
     endif()
+
     set(ALLOCATOR_LIBRARIES mimalloc)
 endif()
 
 # Pybind11
-if (CHIMAERA_ENABLE_PYTHON)
+if(CHIMAERA_ENABLE_PYTHON)
     find_package(pybind11 REQUIRED)
     set(OPTIONAL_LIBS pybind11::embed)
 endif()
 
-#-----------------------------------------------------------------------------
+# -----------------------------------------------------------------------------
 # GPU Support Code
-#-----------------------------------------------------------------------------
+# -----------------------------------------------------------------------------
 
 # ENABLE GPU SUPPORT
 message("CHIMAERA_ENABLE_CUDA: ${CHIMAERA_ENABLE_CUDA}")
-if (CHIMAERA_ENABLE_CUDA)
+
+if(CHIMAERA_ENABLE_CUDA)
     hshm_enable_cuda(17)
 endif()
-if (CHIMAERA_ENABLE_ROCM)
+
+if(CHIMAERA_ENABLE_ROCM)
     hshm_enable_rocm("HIP" 17)
 endif()
 
@@ -77,11 +82,12 @@ endif()
 macro(add_chimod_library namespace target)
     # Create the loadable chimod runtime library
     set(${namespace}_${target}_exports)
-    if (CHIMAERA_ENABLE_CUDA)
+
+    if(CHIMAERA_ENABLE_CUDA)
         add_cuda_library(${namespace}_${target} SHARED TRUE ${ARGN})
         target_link_libraries(${namespace}_${target} PUBLIC hshm::cudacxx)
         target_compile_definitions(${namespace}_${target} PUBLIC CHIMAERA_ENABLE_CUDA)
-    elseif (CHIMAERA_ENABLE_ROCM)
+    elseif(CHIMAERA_ENABLE_ROCM)
         add_rocm_gpu_library(${namespace}_${target} SHARED TRUE ${ARGN})
         target_link_libraries(${namespace}_${target} PUBLIC hshm::rocmcxx_gpu)
         target_compile_definitions(${namespace}_${target} PUBLIC CHIMAERA_ENABLE_ROCM)
@@ -89,10 +95,12 @@ macro(add_chimod_library namespace target)
         add_library(${namespace}_${target} ${ARGN})
         target_link_libraries(${namespace}_${target} PUBLIC hshm::cxx)
     endif()
-    if (CHIMAERA_IS_MAIN_PROJECT)
+
+    if(CHIMAERA_IS_MAIN_PROJECT)
         add_library(${namespace}::${target} ALIAS ${namespace}_${target})
         add_dependencies(${namespace}_${target} chimaera::runtime)
     endif()
+
     target_link_libraries(${namespace}_${target} PUBLIC chimaera::runtime)
     list(APPEND ${namespace}_${target}_exports ${namespace}_${target})
     add_library(${target} INTERFACE)
@@ -101,14 +109,14 @@ macro(add_chimod_library namespace target)
 
     # Add chimod library no gpu client interface
     add_library(${namespace}_${target}_client INTERFACE)
-    target_link_libraries(${namespace}_${target}_client INTERFACE chimaera::client)
+    target_link_libraries(${namespace}_${target}_client INTERFACE chimaera::client_host)
     list(APPEND ${namespace}_${target}_exports ${namespace}_${target}_client)
     add_library(${target}_client INTERFACE)
     target_link_libraries(${target}_client INTERFACE ${namespace}_${target}_client)
     list(APPEND ${namespace}_${target}_exports ${target}_client)
 
     # Add chimod library with cuda support
-    if (CHIMAERA_ENABLE_CUDA)
+    if(CHIMAERA_ENABLE_CUDA)
         add_library(${namespace}_${target}_client_gpu INTERFACE)
         target_link_libraries(${namespace}_${target}_client_gpu INTERFACE hshm::cudacxx chimaera::client_gpu)
         target_include_directories(${namespace}_${target}_client_gpu INTERFACE ${CMAKE_INSTALL_PREFIX}/include)
@@ -120,7 +128,7 @@ macro(add_chimod_library namespace target)
     endif()
 
     # Add chimod library with rocm support
-    if (CHIMAERA_ENABLE_ROCM)
+    if(CHIMAERA_ENABLE_ROCM)
         add_library(${namespace}_${target}_client_gpu INTERFACE)
         target_link_libraries(${namespace}_${target}_client_gpu INTERFACE hshm::rocmcxx_gpu chimaera::client_gpu)
         target_include_directories(${namespace}_${target}_client_gpu INTERFACE ${CMAKE_INSTALL_PREFIX}/include)
---
+++ test/unit/external/tasks/compressor/src/compressor.cc
@@ -0,0 +1,57 @@
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+ * Distributed under BSD 3-Clause license.                                   *
+ * Copyright by The HDF Group.                                               *
+ * Copyright by the Illinois Institute of Technology.                        *
+ * All rights reserved.                                                      *
+ *                                                                           *
+ * This file is part of Hermes. The full Hermes copyright notice, including  *
+ * terms governing use, modification, and redistribution, is contained in    *
+ * the COPYING file, which can be found at the top directory. If you do not  *
+ * have access to the file, you may request a copy from help@hdfgroup.org.   *
+ * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+
+#include "compressor/compressor.h"
+
+#include "chimaera/api/chimaera_runtime.h"
+#include "chimaera/monitor/monitor.h"
+#include "chimaera_admin/chimaera_admin.h"
+
+namespace chi::compressor {
+
+class Server : public Module {
+ public:
+  CLS_CONST LaneGroupId kDefaultGroup = 0;
+
+ public:
+  Server() = default;
+
+  /** Construct compressor */
+  void Create(CreateTask *task, RunContext &rctx) {
+    // Create a set of lanes for holding tasks
+    CreateLaneGroup(kDefaultGroup, 1, QUEUE_LOW_LATENCY);
+    task->SetModuleComplete();
+  }
+  void MonitorCreate(MonitorModeId mode, CreateTask *task, RunContext &rctx) {}
+
+  /** Route a task to a lane */
+  Lane *MapTaskToLane(const Task *task) override {
+    // Route tasks to lanes based on their properties
+    // E.g., a strongly consistent filesystem could map tasks to a lane
+    // by the hash of an absolute filename path.
+    return GetLaneByHash(kDefaultGroup, task->prio_, 0);
+  }
+
+  /** Destroy compressor */
+  void Destroy(DestroyTask *task, RunContext &rctx) {
+    task->SetModuleComplete();
+  }
+  void MonitorDestroy(MonitorModeId mode, DestroyTask *task, RunContext &rctx) {
+  }
+
+ public:
+#include "compressor/compressor_lib_exec.h"
+};
+
+}  // namespace chi::compressor
+
+CHI_TASK_CC(chi::compressor::Server, "compressor");
