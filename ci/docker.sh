05eed082242606e7cadbefac351bc9f431149f4f
lukemartinlogan <lukemartinlogan@gmail.com>
2025-02-13 03:29:18 -0600

    Begin docker

--- ci/docker.sh
+++ ci/docker.sh
@@ -1,6 +1,8 @@
-VERSION=v1.0.0
+
 docker build -t chimaera docker -f docker/deps.Dockerfile
-docker tag chimaera lukemartinlogan/chimaera-deps:${VERSION}
-docker push lukemartinlogan/chimaera-deps:${VERSION}
 docker tag chimaera lukemartinlogan/chimaera-deps:latest
 docker push lukemartinlogan/chimaera-deps:latest
+
+VERSION=v1.0.0
+docker tag chimaera lukemartinlogan/chimaera-deps:${VERSION}
+docker push lukemartinlogan/chimaera-deps:${VERSION}
