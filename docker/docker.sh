dc6a7b196db9fd00d18df94d4bcf173fa6561001
lukemartinlogan <lukemartinlogan@gmail.com>
2025-11-04 15:46:57 -0600

    Do not rebuild in docker compose

--- docker/docker.sh
+++ docker/docker.sh
@@ -6,5 +6,7 @@
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 PROJECT_ROOT="$( cd "${SCRIPT_DIR}/.." && pwd )"
 
+echo $PROJECT_ROOT
+echo $SCRIPT_DIR
 # Build the Docker image
 docker build -t iowarp/iowarp-runtime:latest -f "${SCRIPT_DIR}/Dockerfile.build" "${PROJECT_ROOT}"
